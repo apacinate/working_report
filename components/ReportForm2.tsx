339703eab2d99976b6722aff297cc83a9e218f29
apacinate <apacinate@outlook.jp>
2025-05-08 09:28:44 +0900

    不要箇所をコメントアウト

--- components/ReportForm2.tsx
+++ components/ReportForm2.tsx
@@ -29,14 +29,14 @@ export function ReportForm2() {
     sigCanvas.current?.clear();
   };
 
-  const items = [
-    { code: "A01", reason: "清掃不足（パウダー）" },
-    { code: "A02", reason: "清掃不足（ミルク）" },
-    { code: "A03", reason: "清掃不足（FS）" },
-    { code: "B01", reason: "初期不良（設置1ヶ月未満）" },
-    { code: "C01", reason: "経年劣化" },
-    { code: "D01", reason: "定期点検・改造・撤去・交換など" },
-  ];
+//   const items = [
+//     { code: "A01", reason: "清掃不足（パウダー）" },
+//     { code: "A02", reason: "清掃不足（ミルク）" },
+//     { code: "A03", reason: "清掃不足（FS）" },
+//     { code: "B01", reason: "初期不良（設置1ヶ月未満）" },
+//     { code: "C01", reason: "経年劣化" },
+//     { code: "D01", reason: "定期点検・改造・撤去・交換など" },
+//   ];
 
   return (
     <div className="relative max-w-4xl mx-auto border shadow">
@@ -60,7 +60,7 @@ export function ReportForm2() {
 
       {/* 入力UIと操作 */}
       <div className="absolute top-0 left-0 p-4 w-full">
-        <select
+        {/* <select
           className="border p-1 mb-2 w-full"
           value={reasonCode}
           onChange={(e) => setReasonCode(e.target.value)}
@@ -83,7 +83,7 @@ export function ReportForm2() {
           <option value="B">再販使用品・初期不良</option>
           <option value="D">再販使用品・部品レベル</option>
           <option value="X">法定廃品</option>
-        </select>
+        </select> */}
 
         <button
           onClick={clearSignature}
